4b3342f8f171cb979a1dbd52d46eba87bb44191c
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-03-10 10:36:41 -0700

    feat[cmp]: dont confirm with enter unless selected

--- .config/nvim/lua/b/lspconfig.lua
+++ .config/nvim/lua/b/lspconfig.lua
@@ -49,7 +49,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),	
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),	
   }),
   
 	formatting = {
