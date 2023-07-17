3a5d7c3d0219ecd1641353317b0d0ba664b46808
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-07-16 23:08:52 -0600

    chore: rm rubocop in null-ls

--- .config/nvim/lua/b/null-ls.lua
+++ .config/nvim/lua/b/null-ls.lua
@@ -9,9 +9,6 @@ null_ls.setup({
 
 		null_ls.builtins.formatting.prettier,
 
-		null_ls.builtins.diagnostics.rubocop,
-		null_ls.builtins.formatting.rubocop,
-
 		null_ls.builtins.formatting.rustfmt,
 	},
 	on_attach = function(client, bufnr)
