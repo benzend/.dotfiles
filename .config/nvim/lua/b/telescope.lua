4609809b8115aeeff695baeb4e89c9701f9dc4ca
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-08-11 20:52:10 -0600

    feat: update dotfiles

--- .config/nvim/lua/b/telescope.lua
+++ .config/nvim/lua/b/telescope.lua
@@ -1,18 +1,22 @@
 local lga_actions = require("telescope-live-grep-args.actions")
 
-require('telescope').setup{
-  extensions = {
-    live_grep_args = {
-      auto_quoting = true,
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+		},
+	},
+	extensions = {
+		live_grep_args = {
+			auto_quoting = true,
 
-      mappings = {
-        i = {
-          ["<C-k>"] = lga_actions.quote_prompt(),
-          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-        }
-      }
-    }
-
-  }
-}
-require('telescope').load_extension('project')
+			mappings = {
+				i = {
+					["<C-k>"] = lga_actions.quote_prompt(),
+					["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+				},
+			},
+		},
+	},
+})
+require("telescope").load_extension("project")
